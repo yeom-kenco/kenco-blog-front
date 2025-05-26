792b11370818ca04b3718415db593b827f2886d3
Yeom-Seung-Ah <ysa2780@naver.com>
2025-05-26 13:34:50 +0900

    Feat: 글 목록 페이지 api 연결 (홈 화면)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+// src/pages/HomePage.jsx
+import { useEffect, useState } from 'react'
+import axios from '../api/axios'
+import { Link } from 'react-router-dom'
 
-const HomePage = () => {
-  return <div></div>
+export default function HomePage() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get('/posts')
+        setPosts(res.data)
+      } catch (err) {
+        console.error('글 목록 불러오기 실패', err)
+      }
+    }
+
+    fetchPosts()
+  }, [])
+
+  return (
+    <div>
+      <h2>게시글 목록</h2>
+      <ul>
+        {posts.map(post => (
+          <li key={post._id}>
+            <Link to={`/posts/${post._id}`}>
+              <h3>{post.title}</h3>
+            </Link>
+            <p>작성자: {post.author.username}</p>
+            <p>작성일: {new Date(post.createdAt).toLocaleDateString()}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
-
-export default HomePage
