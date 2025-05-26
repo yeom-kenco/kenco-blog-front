c2ee5501149d731bbef29a5ef0225c79e7d71081
Yeom-Seung-Ah <ysa2780@naver.com>
2025-05-26 11:31:14 +0900

    Build: 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import WritePage from './pages/WritePage'
+import PostDetailPage from './pages/PostDetailPage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
-  return <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/posts/:id" element={<PostDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
